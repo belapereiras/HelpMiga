e733a444b61e65b2afb0f28816e02bd8d4a5338a
belapereiras <isabela_94@msn.com>
2016-07-01 15:43:16 -0300

    map testar

--- MapKitTutorial/Tela_Botao.swift
+++ MapKitTutorial/Tela_Botao.swift
@@ -78,8 +78,8 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let center = CLLocationCoordinate2D(latitude: Location.sharedInstace.lastLocation.coordinate.latitude, longitude: Location.sharedInstace.lastLocation.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.0075, longitudeDelta: 0.0075))
         self.mapView.setRegion(region, animated: true)
-        self.mapView.setRegion(region, animated: true)
         self.mapView.showsUserLocation = true
+        
         
         
         
